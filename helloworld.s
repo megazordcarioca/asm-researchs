fd5cd26607572a3300ec79614e9fc057a40e6568
Joey <josiasdsj1@gmail.com>
2022-05-16 11:05:00 -0300

    Update comments
     but idk if i understand everything

--- helloworld.s
+++ helloworld.s
@@ -2,19 +2,19 @@
 section .text
 global _start
 
-_start:
+                        ; PROGRAMMING WITH INTEL REGISTERS
+_start:                 ; CREATE PROGRAM STRUCTURE
         mov     edx,len
         mov     ecx,msg
-        mov     ebx,1
-        mov     eax,4
+        mov     ebx,1   ;; ARGUMENT TO EBX POINTER
+        mov     eax,4   ;; ACCUMULATOR
 
     ;WRITE IN SYSTEM
-        int     0x80 ;RESERVE INT IN MEMORY CELL
+        int     0x80    ; OPEN STRUCTURE PROGRAM
 
-        mov     eax,1 
-    ;EXIT SYSTEM
+        mov     eax,1   ; MOVE 1 BIT TO ACCUMULATOR FOR EXIT
     
-        int     0x80
+        int     0x80    ; END PROGRAM STRUCTURE
 
 section     .data
         msg     db  'Hello World',0xa
